a640acc781c324cc74946ab885eb85e3d0439317
Aaron Wagner <aaron.wagner@smawandi.com>
2022-09-30 10:37:47 +0200

    nginx reverse proxy

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,19 +1,8 @@
-# define images
-SPRING=spring_api
-NGINX=nginx_proxy
-
 # stop services
-docker-compose rm -sv -f $SPRING
-docker-compose rm -sv -f $NGINX
+docker-compose -f docker-compose.yml down
 
-# clean images
-docker image rm $SPRING
-docker image rm $NGINX
+# clean host
 docker container prune -f
-
-# pull new images
-#docker build -t $SPRING ../.
-#docker build -t $NGINX nginx/.
 
 # start services
 docker-compose -f docker-compose.yml up -d
